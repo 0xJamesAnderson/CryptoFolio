cb0ca5beaa66de18614423e70352b1ac78a3b881
0xJamesAnderson <yhvi8d71w469q@hotmail.com>
2025-01-17 19:22:53 +0800

    integrate components in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CoinList from './components/CoinList';
+import AddCoin from './components/AddCoin';
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const handleAddCoin = (newCoin) => {
+    setCoins([...coins, newCoin]);
+  };
+
   return (
     <div>
       <h1>CryptoFolio</h1>
       <p>Your personal crypto portfolio tracker</p>
+      <AddCoin onAddCoin={handleAddCoin} />
+      <CoinList coins={coins} />
     </div>
   );
 }
